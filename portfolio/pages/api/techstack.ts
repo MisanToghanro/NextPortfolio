1fb958d21d54809fc9e8137e517095c789cda20f
MisanToghanro <tmisan657@gmail.com>
2025-11-12 00:53:57 +0100

    modified techstack.ts

--- portfolio/pages/api/techstack.ts
+++ portfolio/pages/api/techstack.ts
@@ -3,6 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const technologies = [
     {
-        
+
     }
 ]
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json(technologies);
+}
