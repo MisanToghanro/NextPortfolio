a8ab656fb07fa6a9bf899b1f94227a54bd054e1c
MisanToghanro <tmisan657@gmail.com>
2025-11-22 12:21:58 +0100

    refactor: update layout and project card UI + swit ch to optimized logo format
    - Updated Layout and ProjectCards components for improved UI and consistency
    - Adjusted _document.tsx for layout behavior
    - Replaced Logo.jpg with optimized Logo.png
    - Refined global styles in globals.css

--- portfolio/Components/Layout/layout.tsx
+++ portfolio/Components/Layout/layout.tsx
@@ -1,15 +1,22 @@
 import React from "react"
 import Footer from "./footer"
 import Header from "./Header"
+import Head from "next/head"
 
 interface LayoutProps{
     children: React.ReactNode
+    title?: string;
+
 }
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
+const Layout: React.FC<LayoutProps> = ({children, title}) => {
 
     return(
         <div className="flex-col">
+      <Head>
+        <title>{title ?? "Misan Toghanro – Frontend Developer"}</title>
+        <meta name="description" content="Frontend Developer Portfolio" />
+      </Head>
             <Header/>
             <main className="grow">
              {children}
--- portfolio/Components/Projects/ProjectCards.tsx
+++ portfolio/Components/Projects/ProjectCards.tsx
@@ -11,7 +11,7 @@ const ProjectCards: React.FC<ProjectCard> = ({ projects }) => {
         Featured Projects
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {projects.map((project) => (
           <div
             key={project.id}
